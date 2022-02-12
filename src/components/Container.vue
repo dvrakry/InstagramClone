cfe3f0cb4928149e1111766dc2f1df786f99602b
rak <dv.rakry@gmail.com>
2022-02-12 23:04:11 +0900

    2022-02-12

---
+++ src/components/Container.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      <Post :인스타데이터="인스타데이터[i]" v-for="(a,i) in 인스타데이터" :key="i"/>
+      <!-- <Post :인스타데이터="인스타데이터[1]"/>
+      <Post :인스타데이터="인스타데이터[2]"/> -->
+  </div>
+</template>
+
+<script>
+
+import Post from './Post.vue'
+
+export default {
+    components : {
+        Post : Post,
+    },
+    props:{
+        인스타데이터 : Array,
+    },
+}
+</script>
+
+<style>
+
+</style>
